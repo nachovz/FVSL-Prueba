17625f23e47bafe06d2f3c97259d1492eb519b1f
ignaciocordobap@gmail.com <ignaciocordobap@gmail.com@c3b25d8b-2c3e-e66a-3d81-67f58cbb725e>
2010-07-19 17:49:25 +0000

    git-svn-id: http://mapsvsl.googlecode.com/svn/trunk@30 c3b25d8b-2c3e-e66a-3d81-67f58cbb725e

---
+++ MAPA_website/App_Code/MAPA_ODS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+
+/// <summary>
+/// Summary description for MAPA_ODS
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class MAPA_ODS : System.Web.Services.WebService {
+
+    public MAPA_ODS () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hello World";
+    }
+    
+}
+
---
+++ MAPA_website/App_Code/VO/AwardVO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for AwardVO
+/// </summary>
+public class AwardVO
+{
+    public int awardId;
+    public String awardName;
+    public int recibido;
+
+	public AwardVO()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
