5ca6308717eb8aadbd6e80bf7e08374e160fd3ba
ignaciocordobap@gmail.com <ignaciocordobap@gmail.com@c3b25d8b-2c3e-e66a-3d81-67f58cbb725e>
2010-07-20 17:52:47 +0000

    git-svn-id: http://mapsvsl.googlecode.com/svn/trunk@34 c3b25d8b-2c3e-e66a-3d81-67f58cbb725e

--- MAPA_website/App_Code/MAPA_ODS.cs
+++ MAPA_website/App_Code/MAPA_ODS.cs
@@ -28,29 +28,65 @@ public class MAPA_ODS : System.Web.Services.WebService {
     }
 
     [WebMethod]
-    public List<EntityVO> getallODS()
+    public List<ODSVO> getODSD(int inods)
     {
         FVSL_LINQDataContext dbcon = new FVSL_LINQDataContext();
 
-        var resultset = dbcon.MAPA_ODS_ALL();
+        List<ODS_ODS> resultset = dbcon.MAPA_ODS_DETAILS(inods).ToList();
 
-        EntityVO aux = new EntityVO();
+        EntityVO aux;
+
         //ODS_ODS ods = new ODS_ODS();
 
-        List<EntityVO> result = new List<EntityVO>();
+        List<ODSVO> result = new List<ODSVO>();
         
         //dbcon.ODS_ODS ods = new System.Data.Linq.Table<ODS_OD>();
         //dbcon.ODS_ODS ods = new System.Data.Linq.Table<ODS_ODS>();
         //result.Add(dbcon.MAPA_ODS_ALL().Cast<EntityVO>);
 
-        
-
-        //foreach (ODS_ODS ods in result)
-        //{
-          //  result.Add(resultset);
-        //}
+        foreach (ODS_ODS ods in resultset)
+        {
+            result.Add(FODS(ods));
+        }
 
         return result;
+    }
+
+    private ODSVO FODS(ODS_ODS odsin)
+    {
+        ODSVO aux = new ODSVO();
+
+        aux.id = odsin.id_ods.ToString();
+        aux.name = odsin.nombre;
+        aux.latitude = odsin.Latitud;
+        aux.longitude = odsin.Longitud;
+        aux.direction = odsin.ciudad + ", " + odsin.urbanizacion + " " + odsin.calle;
+        aux.website = odsin.pagina_web;
+        aux.facebook = odsin.FacebookODS;
+        aux.twitter = odsin.TwitterODS;
+        aux.objective = odsin.objetivos;
+        aux.email = odsin.email;
+        aux.type = "ods";
+        aux.beneficiarios = getODSbeneficiario(odsin.id_ods);
+
+        return aux;
+
+    }
+
+    private List<String> getODSbeneficiario(int id)
+    {
+        List<String> lista = new List<string>();
+        FVSL_LINQDataContext dbaux = new FVSL_LINQDataContext();
+
+        List<beneficiario> benefs = dbaux.MAPA_GET_BENF_ODS(id).ToList();
+
+        foreach (beneficiario ben in benefs)
+        {
+            lista.Add(ben.nombre);
+        }
+
+        return lista;
+
     }
     
 }
