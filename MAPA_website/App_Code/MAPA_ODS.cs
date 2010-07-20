e7e14f0973e414f4bbb2d575b4ce4174b27da55b
ignaciocordobap@gmail.com <ignaciocordobap@gmail.com@c3b25d8b-2c3e-e66a-3d81-67f58cbb725e>
2010-07-20 15:47:29 +0000

    git-svn-id: http://mapsvsl.googlecode.com/svn/trunk@33 c3b25d8b-2c3e-e66a-3d81-67f58cbb725e

--- MAPA_website/App_Code/MAPA_ODS.cs
+++ MAPA_website/App_Code/MAPA_ODS.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Services;
 using System.Web.Services.Protocols;
 using System.Xml.Linq;
+using System.Collections.Generic;
 
 /// <summary>
 /// Summary description for MAPA_ODS
@@ -24,6 +25,32 @@ public class MAPA_ODS : System.Web.Services.WebService {
     [WebMethod]
     public string HelloWorld() {
         return "Hello World";
+    }
+
+    [WebMethod]
+    public List<EntityVO> getallODS()
+    {
+        FVSL_LINQDataContext dbcon = new FVSL_LINQDataContext();
+
+        var resultset = dbcon.MAPA_ODS_ALL();
+
+        EntityVO aux = new EntityVO();
+        //ODS_ODS ods = new ODS_ODS();
+
+        List<EntityVO> result = new List<EntityVO>();
+        
+        //dbcon.ODS_ODS ods = new System.Data.Linq.Table<ODS_OD>();
+        //dbcon.ODS_ODS ods = new System.Data.Linq.Table<ODS_ODS>();
+        //result.Add(dbcon.MAPA_ODS_ALL().Cast<EntityVO>);
+
+        
+
+        //foreach (ODS_ODS ods in result)
+        //{
+          //  result.Add(resultset);
+        //}
+
+        return result;
     }
     
 }
