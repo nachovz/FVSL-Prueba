9138c97590c573366c2a8e538c8c96d5fba016ba
alex.milano <alex.milano@c3b25d8b-2c3e-e66a-3d81-67f58cbb725e>
2010-08-13 22:43:10 +0000

    EMPTemplate.cs
    git-svn-id: http://mapsvsl.googlecode.com/svn/trunk@77 c3b25d8b-2c3e-e66a-3d81-67f58cbb725e

---
+++ asp-workspace/MAPA_FVSL/PDFExport/PDFExport/Templates/EMPTemplate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Root.Reports;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PDFExport.Templates;
+using MAP;
+
+namespace PDFExport
+{
+    public class EMPTemplate : BaseTemplate
+    {
+        private CompanyVO _emp;
+        public EMPTemplate(CompanyVO emp)
+        {
+            this.Id = BaseTemplate.COMPANY_TEMPLATE;
+            this._emp = emp;
+        }
+
+        public CompanyVO emp { get { return _emp; } }
+
+        override public void setContent()
+        {
+
+            this.addTitle(37, 18, "Informacion de Empresa");
+            this.addContentLine(37, 27, emp.name);
+            this.addLeftContent(emp.objective);
+            this.addRightContent(emp.direction, emp.website, emp.email, emp.facebook, emp.twitter);
+            
+            
+        }
+
+    }
+}
