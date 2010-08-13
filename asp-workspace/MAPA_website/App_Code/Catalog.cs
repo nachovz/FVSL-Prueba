d9c8b37d435e9f7ba589851615da6b1bf48dce08
ignaciocordobap@gmail.com <ignaciocordobap@gmail.com@c3b25d8b-2c3e-e66a-3d81-67f58cbb725e>
2010-08-13 17:39:54 +0000

    git-svn-id: http://mapsvsl.googlecode.com/svn/trunk@61 c3b25d8b-2c3e-e66a-3d81-67f58cbb725e

---
+++ asp-workspace/MAPA_website/App_Code/Catalog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using MAP;
+
+/// <summary>
+/// Summary description for Catalog
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class Catalog : System.Web.Services.WebService {
+
+    public Catalog () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public List<CataloValueVO> getCatalog(String catalogo, int p) {
+
+        return CatalogExtractor.create(catalogo).getCatalog(p);
+    }
+}
---
+++ asp-workspace/MAPA_website/App_Code/Network.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using MAP;
+/// <summary>
+/// Summary description for Network
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class Network : System.Web.Services.WebService {
+
+    public Network () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public List<NetworkVO> getN(String type)
+    {
+        return NetworkExtractor.create(type).getN();
+    }
+
+    [WebMethod]
+    public List<NetworkVO> getSearch(String type, List<String> data)
+    {
+        if (data.Count >= 2)
+        {
+            return NetworkExtractor.create(type).getSearch(data);
+        }
+        else
+        {
+            Logging.WriteError("Los datos de entrada fueron insuficientes");
+            return null;
+        }
+    }
+
+    [WebMethod]
+    public NetworkVO getDetails(String type, int padre)
+    {
+        return NetworkExtractor.create(type).getDetails(padre);
+    }
+    
+}
+
